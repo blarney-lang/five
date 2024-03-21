aa1e5bb37c6efcf72e43f1028ababa6ea556af4b
Matthew Naylor <mn416@cam.ac.uk>
2024-03-21 13:08:26 +0000

    WIP

--- src/Blarney/Five/Verify.hs
+++ src/Blarney/Five/Verify.hs
@@ -12,118 +12,76 @@ import Blarney.Five.RegisterFile
 import Blarney.Five.BranchPredictor
 
 -- Type parameters for verification
-type V_XLen    = 3   -- Register width
-type V_LogRegs = 2   -- Log_2 of number of registers
+type XLen = 3     -- Three-bit registers
+type ILen = XLen  -- Instruction width same as register width
+type LogRegs = 2  -- Four registers in total
 
--- Memory request type for verification
-data V_MemReq =
-  V_MemReq {
-    -- Unique request id
-    uid :: Bit V_XLen
-    -- Do we expect a response from this request?
-  , hasResp :: Bit 1
-  }
-  deriving (Generic, Bits)
-
--- The map-filter server consumes requests, filters out requests that
--- don't match a given predicate, applies a given function to convert a
--- request into a response, and returns back the responses.  It uses
--- universally quantified put and peek masks to capture arbitrary latency
--- and backpressure. If the masks are always true, the server is capable
--- of operating at full throughput, consuming a request and producing a
--- response on every cycle. The number of outstanding requests is
--- controlled by the size of the internal queue.
-makeMapFilterServer :: (Bits req, Bits resp)
-                    => Bit 1 -> Bit 1
-                    -> (req -> Bit 1)
-                    -> (req -> resp)
-                    -> Module (Server req resp)
-makeMapFilterServer putMask peekMask p f = do
-  q <- makePipelineQueue 1
-  let src = Source { canPeek = q.canDeq .&&. peekMask
-                   , peek    = q.first
-                   , consume = q.deq }
-  let snk = Sink { canPut = q.notFull .&&. putMask
-                 , put = \req ->
-                     when (p req) do q.enq (f req) }
-  return (Server snk src)
-
--- Instruction format for verification
-data V_Instr =
-  V_Instr {
-    uid :: Bit V_XLen
-  , rd  :: Option (Bit V_LogRegs)
-  , rs1 :: Option (Bit V_LogRegs)
-  , rs2 :: Option (Bit V_LogRegs)
+-- Decoded instructions hold the raw (encoded) instruction,
+-- the operands, a memory-access bit, and a can-branch bit
+data Instr = Instr {
+    raw :: Bit ILen
+  , rd :: Option (Bit LogRegs)
+  , rs1 :: Option (Bit LogRegs)
+  , rs2 :: Option (Bit LogRegs)
   , isMemAccess :: Bit 1
   , canBranch :: Bit 1
   }
   deriving (Generic, Bits)
 
--- Instruction set interface for verification
-v_instrSet execute =
-  InstrSet {
-    getDest       = \i -> i.rd
-  , getSrcs       = \i -> (i.rs1, i.rs2)
-  , isMemAccess   = \i -> i.isMemAccess
-  , decode        = \uid ->
-      V_Instr {
-        uid = uid
-      , rd  = Option (var "rd_valid")  (var "rd")
-      , rs1 = Option (var "rs1_valid") (var "rs1")
-      , rs2 = Option (var "rs2_valid") (var "rs2")
-      , isMemAccess = var "is_mem_access"
-      , canBranch = var "can_branch"
-      }
-  , execute     = execute
-  , incPC       = 1
-  , canBranch   = \i -> i.canBranch
-  }
+-- Data memory requests have an address and may expect a response
+data MReq = MReq { addr :: Bit XLen, hasResp :: Bit 1 }
+  deriving (Generic, Bits)
 
--- Execution unit for verification
-makeGoldenExecUnit :: Bit V_XLen -> Bit V_XLen -> Bool ->
-  Module (V_Instr -> ExecState V_XLen V_MemReq -> Action ())
-makeGoldenExecUnit initPC instrLen enAsserts = do
-  goldenPC   <- makeReg initPC
-  goldenRegs <- replicateM (2 ^ valueOf @V_LogRegs) (makeReg 0)
-  stall <- makeWire false
-
-  return \instr s -> do
-    -- Check that correct instruction has been supplied
-    when enAsserts do
-      assert (instr.uid .==. s.pc.val) "Instruction correct"
-
-    -- Issue memory request
-    let req = var "req"
-    when instr.isMemAccess do
-      s.memReq <== V_MemReq { uid = req, hasResp = instr.rd.valid }
-
-    -- Optionally perform a branch
-    let branch = instr.canBranch .&&. var "branch_valid"
-    let branchTarget = var "branch_target"
-    when branch do s.pc <== branchTarget
-    -- Check and maintain golden PC
-    when enAsserts do
-      assert (s.pc.val .==. goldenPC.val) "PC correct"
-    when (inv stall.val) do
-      goldenPC <== if branch then branchTarget
-                     else goldenPC.val + instrLen
-
-    -- Write result and update golden register file
-    when instr.rd.valid do
-      let result = var "result"
-      s.result <== result
-      goldenRegs ! instr.rd.val <==
-        if instr.isMemAccess then req else result
-
-    -- Check operands against golden register file
-    let operandsOk =
-          andList [ r.valid .==>. (goldenRegs!r.val).val .==. o
-                  | (r, o) <- zip [instr.rs1, instr.rs2]
-                                  [fst s.operands, snd s.operands] ]
-    when enAsserts do
-      assert operandsOk "Operands correct"
-
+-- An "instruction set" that checks pipeline properties
+makeChecker :: Bool -> Module (InstrSet XLen ILen Instr LogRegs MReq)
+makeChecker enAsserts = do
+  goldenPC <- makeReg 0
+  goldenRegs <- replicateM (2 ^ valueOf @LogRegs) (makeReg 0)
+  return
+    InstrSet {
+      decode = \raw ->
+        Instr {
+          raw = raw
+        , rd  = Option (var "rd_valid")  (var "rd")
+        , rs1 = Option (var "rs1_valid") (var "rs1")
+        , rs2 = Option (var "rs2_valid") (var "rs2")
+        , isMemAccess = var "is_mem_access"
+        , canBranch = var "can_branch"
+        }
+    , getDest = \i -> i.rd
+    , getSrcs = \i -> (i.rs1, i.rs2)
+    , execute = \i s -> do
+        -- Check raw instruction matches the PC
+        when enAsserts do
+          assert (i.raw .==. s.pc.val) "Instruction correct"
+        -- Optionally perform a branch
+        let branchValid = i.canBranch .&&. var "branch_valid"
+        let branchTarget = var "branch_target"
+        when branchValid do s.pc <== branchTarget
+        -- Maintain golden PC and check against it
+        goldenPC <== if branchValid then branchTarget
+                       else goldenPC.val + 1
+        when enAsserts do
+          assert (s.pc.val .==. goldenPC.val) "PC correct"
+        -- Issue memory request
+        let mreq = MReq { addr = var "addr", hasResp = i.rd.valid }
+        when i.isMemAccess do s.memReq <== mreq
+        -- Write result and update golden register file
+        when i.rd.valid do
+          let result = var "result"
+          s.result <== result
+          goldenRegs!i.rd.val <==
+            if i.isMemAccess then mreq.addr else result
+        -- Check operands against golden register file
+        let check r x = r.valid .==>. (goldenRegs!r.val).val .==. x
+        when enAsserts do
+          assert (check i.rs1 (fst s.operands) .&&.
+                  check i.rs2 (snd s.operands)) "Operands correct"
+    , isMemAccess = \i -> i.isMemAccess
+    , incPC = 1
+    , canBranch = \i -> i.canBranch
+    }
+           
 -- No consecutive branch mispredictions
 checkNoConsecutiveMispreds s = do
   -- The number of consecutive branch mispredictions
@@ -153,6 +111,29 @@ checkForwardProgress n t s = do
     assert (time.val .>=. t .==>. retired.val .>=. fromInteger n)
            ("Forward progress " ++ show n)
 
+-- The map-filter server consumes requests, filters out requests that
+-- don't match a given predicate, applies a given function to convert a
+-- request into a response, and returns back the responses.  It uses
+-- universally quantified put and peek masks to capture arbitrary latency
+-- and backpressure. If the masks are always true, the server is capable
+-- of operating at full throughput, consuming a request and producing a
+-- response on every cycle. The number of outstanding requests is
+-- controlled by the size of the internal queue.
+makeMapFilterServer :: (Bits req, Bits resp)
+                    => Bit 1 -> Bit 1
+                    -> (req -> Bit 1)
+                    -> (req -> resp)
+                    -> Module (Server req resp)
+makeMapFilterServer putMask peekMask p f = do
+  q <- makePipelineQueue 1
+  let src = Source { canPeek = q.canDeq .&&. peekMask
+                   , peek    = q.first
+                   , consume = q.deq }
+  let snk = Sink { canPut = q.notFull .&&. putMask
+                 , put = \req ->
+                     when (p req) do q.enq (f req) }
+  return (Server snk src)
+
 -- Pipeline for correctness verification
 makeCorrectnessVerifier :: Module ()
 makeCorrectnessVerifier = mdo
@@ -161,9 +142,8 @@ makeCorrectnessVerifier = mdo
                               (const true) id
   dmem <- makeMapFilterServer (var "dmem_put_mask")
                               (var "dmem_peek_mask")
-                              (.hasResp) (.uid)
-  exec  <- makeGoldenExecUnit 0 1 True
-  let iset = v_instrSet exec
+                              (.hasResp) (.addr)
+  iset <- makeChecker True
   branchPred <- makeArbitraryPredictor iset
   regFile <- makeRegisterFile
   let params = 
@@ -180,10 +160,9 @@ makeCorrectnessVerifier = mdo
 -- Pipeline for forward progress verification
 makeForwardProgressVerifier :: Int -> Int -> Module ()
 makeForwardProgressVerifier n d = mdo
-  imem  <- makeMapFilterServer true true (const true) id
-  dmem  <- makeMapFilterServer true true (.hasResp) (.uid)
-  exec  <- makeGoldenExecUnit 0 1 False
-  let iset = v_instrSet exec
+  imem <- makeMapFilterServer true true (const true) id
+  dmem <- makeMapFilterServer true true (.hasResp) (.addr)
+  iset <- makeChecker False
   branchPred <- makeArbitraryPredictor iset
   regFile <- makeRegisterFile
   let params =
